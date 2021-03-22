f9a88bf41df18dfb59882b2b43529e0b637e99a6
Mellieejj <Melanieverheul1992@gmail.com>
2021-03-22 15:47:00 +0100

    gevangenis en terug-naar-start velden toegevoegd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
 class BordSpel {
     public int huidigePlaats = 0;
-    public final int finishVeld = 64;
+    public final int finishVeld = 63;
     public int aantalOgen;
     public final Random random = new Random();
 
@@ -30,30 +30,35 @@ class BordSpel {
     }
 
     public void checkVeld(int veld) {
-        if (veld == 10 || veld == 20 || veld == 30 || veld == 40 || veld == 50 || veld == 60){
-            int plaats = veld;
+        if (veld == 10 || veld == 20 || veld == 30 || veld == 40 || veld == 50 || veld == 60) {
             huidigePlaats += aantalOgen;
-
-            System.out.println("Je hebt " + aantalOgen + " gegooid. Je staat op plaats " + plaats + ". BONUS STAPJES! Je staat op plaats "+ huidigePlaats );
+            System.out.println("Je hebt " + aantalOgen + " gegooid. Je staat op plaats " + veld + ". BONUS STAPJES! Je staat op plaats " + huidigePlaats);
+        } else if (veld == 25 || veld == 45) {
+            huidigePlaats = 0;
+            System.out.println("Je hebt " + aantalOgen + " gegooid. Je staat op plaats " + veld + ". Terug naar start.");
         } else {
-            System.out.println("Je hebt " + aantalOgen + " gegooid. Je staat op plaats " + huidigePlaats + ".");
+            System.out.println("Je hebt " + aantalOgen + " gegooid. Je staat op plaats " + veld + ".");
         }
     }
 
     public void spel() {
-        try(Scanner scan = new Scanner(System.in)){
-            while(huidigePlaats < finishVeld) {
-                System.out.println("\nGooi je dobbelsteen (g).");
-                String input = scan.next();
-                if (input.equals("g")) {
-                    dobbelsteenWorp();
-                    huidigePlaats += aantalOgen;
-                }
-                checkVeld(huidigePlaats);
+        Scanner scan = new Scanner(System.in);
+        while (huidigePlaats < finishVeld) {
+            System.out.println("\nGooi je dobbelsteen (g).");
+            String input = scan.next();
+
+            if (input.equals("g")) {
+                dobbelsteenWorp();
+                huidigePlaats += aantalOgen;
             }
+            checkVeld(huidigePlaats);
         }
 
-        if (huidigePlaats >= finishVeld){
+        if (huidigePlaats == 23) {
+            System.out.println("Gevangenis! GAME OVER! :-( ");
+        }
+
+        if (huidigePlaats >= finishVeld) {
             System.out.println("Gewonnen!");
         }
     }
